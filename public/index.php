fc790964ebd9b1f0a59c16f04c7cf1bffd5d393d
Abdullah Al Mamun <abdullah.mamun@itconquest.com>
2019-02-22 22:18:10 +0600

    add files

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "Following subdomains are using SSL now from my LocalPC<br><br><br>";
+
+echo "https://somarpon.com<br><br>";
+echo "https://www.somarpon.com<br><br>";
+echo "https://app.somarpon.com<br><br>";
+echo "https://git.somarpon.com<br><br>";
+echo "https://pf.somarpon.com<br><br>";
+?>
